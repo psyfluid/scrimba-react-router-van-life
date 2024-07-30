62129322c0e0344e4af5526166b00e3fbc801243
psyfluid <psyfluid@gmail.com>
2024-07-31 02:54:39 +0300

    add localStorage loggedin item

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 import { redirect } from 'react-router-dom';
 
 export async function requireAuth() {
-  const isLoggedIn = true;
+  const isLoggedIn = JSON.parse(localStorage.getItem('loggedin')) || false;
   if (!isLoggedIn) {
     const response = redirect('/login?message=You must log in first');
     response.body = true;
